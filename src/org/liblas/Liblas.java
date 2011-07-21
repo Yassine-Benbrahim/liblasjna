2e48bd358cf873f1e7caae7b8372a4c1e2ad3bce
whatnickd <whatnickd@localhost>
2011-07-22 00:19:23 +1000

    ENH: Read a point from sample data

--- src/org/liblas/Liblas.java
+++ src/org/liblas/Liblas.java
@@ -7,4 +7,13 @@ public interface Liblas extends com.sun.jna.Library {
 	Liblas lib = (Liblas) Native.loadLibrary("las", Liblas.class);
 	
 	String LAS_GetVersion();
+	String LAS_GetFullVersion();
+	int LAS_IsGDALEnabled();
+	int LAS_IsLibGeoTIFFEnabled();
+	long LASReader_Create(String filename);
+	void LASReader_Destroy(long hFile);
+	long LASReader_GetNextPoint(long hFile);
+	double LASPoint_GetX(long hPoint);  	
+	double LASPoint_GetY(long hPoint);
+	double LASPoint_GetZ(long hPoint);
 }
--- test/org/liblas/test/LiblasTest.java
+++ test/org/liblas/test/LiblasTest.java
@@ -11,5 +11,16 @@ public class LiblasTest extends TestCase {
 	{
 		Native.setProtected(true);
 		System.out.println(Liblas.lib.LAS_GetVersion());
+		//FIXME: Not present in my Ubuntu distro
+		//System.out.println(Liblas.lib.LAS_GetFullVersion());
+		System.out.println(Liblas.lib.LAS_IsGDALEnabled());
+		System.out.println(Liblas.lib.LAS_IsLibGeoTIFFEnabled());
+		long fileH = Liblas.lib.LASReader_Create("samples/lt_srs_rt-qt-1.2.las");
+		System.out.println(fileH);
+		long hPoint = Liblas.lib.LASReader_GetNextPoint(fileH);
+		System.out.println(Liblas.lib.LASPoint_GetX(hPoint));
+		System.out.println(Liblas.lib.LASPoint_GetY(hPoint));
+		System.out.println(Liblas.lib.LASPoint_GetZ(hPoint));
+		Liblas.lib.LASReader_Destroy(fileH);
 	}
 }
